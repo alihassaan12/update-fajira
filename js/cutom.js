e7ee98eb2d2a28f2350ee1cc5a5749ca1143caa4
alihassaan12 <117427984+alihassaan12@users.noreply.github.com>
2023-04-17 17:16:37 +0500

    home page add

---
+++ js/cutom.js
@@ -0,0 +1,73 @@
+//  {------ owl-Carousel-start ------}
+$( '.owl-carousel' ).owlCarousel( {
+    loop: true,
+    margin: 0,
+    nav: false,
+    autoplay: true,
+    dots: false,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 2,
+        },
+        600: {
+            items: 4,
+        },
+        1000: {
+            items: 7,
+        }
+    }
+} )
+//  {------ owl-Carousel-end ------}
+//  {--------- tabs-start ---------}
+const tabs = document.querySelector( ".wrapper" );
+const tabButton = document.querySelectorAll( ".tab-button" );
+const contents = document.querySelectorAll( ".content" );
+
+tabs.onclick = e => {
+    const id = e.target.dataset.id;
+    if ( id )
+    {
+        tabButton.forEach( btn => {
+            btn.classList.remove( "active" );
+        } );
+        e.target.classList.add( "active" );
+
+        contents.forEach( content => {
+            content.classList.remove( "active" );
+        } );
+        const element = document.getElementById( id );
+        element.classList.add( "active" );
+    }
+}
+//  {--------- tabs-end -----------}
+//  {--------- product-tabs-start -----------}
+function Tabs () {
+    var bindAll = function () {
+        var menuElements = document.querySelectorAll( '[data-tab]' );
+        for ( var i = 0; i < menuElements.length; i++ )
+        {
+            menuElements[ i ].addEventListener( 'click', change, false );
+        }
+    }
+    var clear = function () {
+        var menuElements = document.querySelectorAll( '[data-tab]' );
+        for ( var i = 0; i < menuElements.length; i++ )
+        {
+            menuElements[ i ].classList.remove( 'active' );
+            var id = menuElements[ i ].getAttribute( 'data-tab' );
+            document.getElementById( id ).classList.remove( 'active' );
+        }
+    }
+    var change = function ( e ) {
+        clear();
+        e.target.classList.add( 'active' );
+        var id = e.currentTarget.getAttribute( 'data-tab' );
+        document.getElementById( id ).classList.add( 'active' );
+    }
+
+    bindAll();
+}
+var connectTabs = new Tabs();
+//  {--------- product-tabs-end -----------}
